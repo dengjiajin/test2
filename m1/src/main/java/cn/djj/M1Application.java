af94ee0268aa26127c7e9a34d22e138d5395ff02
dengjiajin <493970294@QQ.COM>
2024-08-19 14:48:48 +0800

    1

---
+++ m1/src/main/java/cn/djj/M1Application.java
@@ -0,0 +1,19 @@
+package cn.djj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class M1Application {
+    public static void main(String[] args)  {
+
+        SpringApplication.run(M1Application.class, args);
+
+
+
+
+    }
+}
